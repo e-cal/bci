21cff454d4b7f7ebe6f9911c8a00449827d96365
ecal <ethan@ecal.dev>
2023-03-03 10:42:41 -0500

    Add EEGNet

--- models/full-workflow.sync.py
+++ models/full-workflow.sync.py
@@ -8,18 +8,23 @@ import time
 import numpy as np
 import pandas as pd
 import tensorflow as tf
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
 from brainflow import *
 from keras import layers
 from matplotlib import pyplot as plt
 from scipy import signal
 from sklearn.model_selection import train_test_split
+from torch.utils.data import DataLoader, TensorDataset
 
 # %% [markdown]
 # Training Data
 
 # %%
 raw_training = DataFilter.read_file("../data/training_EEG.csv")
-raw_training2 = DataFilter.read_file("../data/training-long_EEG.csv")
+# raw_training2 = DataFilter.read_file("../data/training-long_EEG.csv")
 # raw_training3 = DataFilter.read_file("../data/combo_file.csv")
 
 # %%
@@ -36,7 +41,7 @@ training_raw_times = raw_training[22][:]
 training_raw_markers = raw_training[23][:]
 
 # %%
-raw_training = DataFilter.read_file("../data/training-long_EEG.csv")
+raw_training = DataFilter.read_file("../data/training_EEG.csv")
 raw_training = pd.DataFrame(np.transpose(raw_training))
 
 training_raw_channels = []
@@ -63,7 +68,7 @@ testing_raw_times = raw_testing[22][:]
 testing_raw_markers = raw_testing[23][:]
 
 # %% [markdown]
-# #Trim and Scale Data
+# # Trim and Scale Data
 # Define scaling factor
 
 # %%
@@ -114,7 +119,7 @@ def bandpass(start, stop, signal_data, fs=250):
 
 
 # %% [markdown]
-# #Apply Filters
+# ### Apply Filters
 #
 
 # %%
@@ -131,7 +136,7 @@ for i in range(8):
     filtered_testing.append(notched)
 
 # %% [markdown]
-# #Fourier Transforms
+# ## Fourier Transforms
 # First we take the fourier transform of the entire dataset, averaging over all channels
 
 # %%
@@ -158,7 +163,7 @@ testing_avg_fourier = np.mean(testing_stacked, axis=1)
 # %% [markdown]
 # Plot FFT of Filtered Training Data
 
-# %% colab={"base_uri": "https://localhost:8080/", "height": 281}
+# %%
 plt.plot(training_freqs, training_avg_fourier)
 plt.xlim(0, 125)
 plt.ylim(0, 3000)
@@ -168,7 +173,7 @@ plt.show()
 # %% [markdown]
 # Plot FFT of Filtered Testing Data
 
-# %% colab={"base_uri": "https://localhost:8080/", "height": 281}
+# %%
 plt.plot(testing_freqs, testing_avg_fourier)
 plt.xlim(0, 125)
 plt.ylim(0, 3000)
@@ -193,8 +198,8 @@ latency = 0.02
 n_width = 6
 
 # Range of frequencies to consider
-freq_lowerbound = 10
-freq_upperbound = 100
+freq_lowerbound = 13
+freq_upperbound = 60
 
 
 # %% [markdown]
@@ -278,7 +283,7 @@ x_valid, y_valid, freq_list = get_processed_data(
     testing_times, testing_channels, testing_spaces
 )
 
-# %% colab={"base_uri": "https://localhost:8080/"}
+# %%
 print(f"x_train shape: {np.shape(x_data)}, y_train shape: {np.shape(y_data)}")
 print(f"x_valid shape: {np.shape(x_valid)}, y_valid shape: {np.shape(y_valid)}")
 print(
@@ -301,11 +306,13 @@ x_train, x_test, y_train, y_test = train_test_split(
     x_data, y_data, test_size=0.2, random_state=42, shuffle=True
 )
 
+# %% [markdown]
+# # Removing 0 data
+
 # %%
 removal_ratio = 99 / 100
 
-# %% colab={"base_uri": "https://localhost:8080/"}
-#### DUMB SHIT TESTING
+# %%
 # Identify data points belonging to class 0
 print(len(x_train))
 class_1_indices = np.where(y_train == 0)[0]
@@ -321,7 +328,7 @@ x_train = np.delete(x_train, points_to_remove, axis=0)
 y_train = np.delete(y_train, points_to_remove, axis=0)
 print(len(x_train))
 
-# %% colab={"base_uri": "https://localhost:8080/"}
+# %%
 # Identify data points belonging to class 0
 print(len(x_test))
 class_1_indices = np.where(y_test == 0)[0]
@@ -337,6 +344,9 @@ x_test = np.delete(x_test, points_to_remove, axis=0)
 y_test = np.delete(y_test, points_to_remove, axis=0)
 print(len(x_test))
 
+# %% [markdown]
+# # Reshape data
+
 # %%
 x_train = x_train.reshape(x_train.shape[0], x_train.shape[1], 1)
 x_test = x_test.reshape(x_test.shape[0], x_test.shape[1], 1)
@@ -344,7 +354,7 @@ x_test = x_test.reshape(x_test.shape[0], x_test.shape[1], 1)
 y_train = y_train.reshape(y_train.shape[0], 1)
 y_test = y_test.reshape(y_test.shape[0], 1)
 
-# %% colab={"base_uri": "https://localhost:8080/"}
+# %%
 print(f"x_train shape: {np.shape(x_train)}, y_train shape: {np.shape(y_train)}")
 print(f"x_test shape: {np.shape(x_test)}, y_test shape: {np.shape(y_test)}")
 print(
@@ -353,6 +363,9 @@ print(
 print(
     f"Minimum validation accuracy = {round(1 - np.sum(y_test) / np.shape(x_test)[0], 4)}"
 )
+
+# %%
+x_train.shape
 
 
 # %% [markdown]
@@ -422,6 +435,9 @@ y_train = y_train.reshape(y_train.shape[0], 1)
 y_test = y_test.reshape(y_test.shape[0], 1)
 
 # %%
+x_train.shape
+
+# %%
 model = tf.keras.Sequential(
     [
         # layers.Reshape((input_dim, 1)),
@@ -450,14 +466,200 @@ model.compile(optimizer="adam", loss=custom_loss, metrics=["accuracy"])
 # %% [markdown]
 # Train model
 
-# %% colab={"base_uri": "https://localhost:8080/"}
+# %%
 print(np.shape(x_train))
 
-# %% colab={"base_uri": "https://localhost:8080/"}
+# %%
 model.fit(x_train, y_train, batch_size=64, epochs=25, validation_data=(x_test, y_test))
 
-# %% colab={"base_uri": "https://localhost:8080/", "height": 283}
+# %%
 plt.plot(model.history.history["val_accuracy"])
+
+# %% [markdown]
+# # Pytorch
+
+# %%
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device.type
+
+# %%
+CHANNELS = 8
+SAMPLE_HZ = 250
+
+
+# %%
+class EEGNet(nn.Module):
+    def __init__(
+        self,
+        n_classes,
+        channels=CHANNELS,
+        sample_hz=SAMPLE_HZ,
+        dropout_rate=0.5,
+        kernel_len=SAMPLE_HZ // 2,
+        tfilter1=8,
+        n_spatial_filters=2,
+        tfilter2=None,  # tfilter1 * n_spatial_filters
+        norm_rate=0.25,
+        dropout_type="Dropout",
+    ):
+        super(EEGNet, self).__init__()
+        self.nb_classes = n_classes
+        self.channels = channels
+        self.sample_hz = sample_hz
+        self.dropout_rate = dropout_rate
+        self.kernel_len = kernel_len
+        self.tfilter1 = tfilter1
+        self.n_spatial_filters = n_spatial_filters
+        if tfilter2 is None:
+            self.tfilter2 = tfilter1 * n_spatial_filters
+        else:
+            self.tfilter2 = tfilter2
+        self.norm_rate = norm_rate
+
+        if dropout_type == "Dropout2D":
+            self.dropout = nn.Dropout2d
+        elif dropout_type == "Dropout":
+            self.dropout = nn.Dropout
+        else:
+            raise ValueError(
+                "dropoutType must be one of Dropout2D or Dropout, passed as a string."
+            )
+
+        self.conv1 = nn.Conv2d(
+            1,
+            self.tfilter1,
+            kernel_size=(1, self.kernel_len),
+            padding=(0, self.kernel_len // 2),
+            bias=False,
+        )
+        self.bn1 = nn.BatchNorm2d(self.tfilter1)
+        self.depthwise = nn.Conv2d(
+            self.tfilter1,
+            self.tfilter1 * self.n_spatial_filters,
+            kernel_size=(self.channels, 1),
+            groups=self.tfilter1,
+            bias=False,
+        )
+        self.bn2 = nn.BatchNorm2d(self.tfilter1 * self.n_spatial_filters)
+        self.separable_conv = nn.Conv2d(
+            self.tfilter1 * self.n_spatial_filters,
+            self.tfilter2,
+            kernel_size=(1, 16),
+            padding=(0, 8),
+            bias=False,
+        )
+        self.bn3 = nn.BatchNorm2d(self.tfilter2)
+        self.flatten = nn.Flatten()
+        self.dense = nn.Linear(self.tfilter2 * 4, self.nb_classes)
+
+    def forward(self, x):
+        print(x.shape)
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = F.elu(x)
+        x = self.depthwise(x)
+        x = self.bn2(x)
+        x = F.elu(x)
+        x = F.avg_pool2d(x, kernel_size=(1, 4))
+        x = self.dropout(p=self.dropout_rate)(x)
+
+        x = self.separable_conv(x)
+        x = self.bn3(x)
+        x = F.elu(x)
+        x = F.avg_pool2d(x, kernel_size=(1, 8))
+        x = self.dropout(p=self.dropout_rate)(x)
+
+        x = self.flatten(x)
+        x = F.linear(
+            x,
+            self.dense.weight
+            * torch.clamp(torch.norm(self.dense.weight), max=self.norm_rate)
+            / torch.norm(self.dense.weight),
+        )
+        x = F.softmax(x, dim=1)
+
+        return x
+
+
+# %%
+# reshape for EEGNet
+x_train = x_train.reshape(x_train.shape[0], x_train.shape[1], 1)
+x_test = x_test.reshape(x_test.shape[0], x_test.shape[1], 1)
+
+y_train = y_train.reshape(y_train.shape[0], 1)
+y_train = y_train.reshape(y_train.shape[0], 1)
+
+# %%
+# convert to torch tensor
+x_train_tensor = torch.from_numpy(x_train).to(device)
+x_test_tensor = torch.from_numpy(x_test).to(device)
+
+y_train_tensor = torch.from_numpy(y_train).to(device)
+y_test_tensor = torch.from_numpy(y_test).to(device)
+
+# %%
+# create dataset
+train_dataset = TensorDataset(x_train_tensor, y_train_tensor)
+test_dataset = TensorDataset(x_test_tensor, y_test_tensor)
+
+# %%
+# create dataloader
+train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=64, shuffle=True)
+
+# %%
+x_train.shape
+
+# %%
+# Define the loss function and optimizer
+
+# create model
+model = EEGNet(n_classes=2).to(device)
+
+# %%
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+# %%
+# Training loop
+
+num_epochs = 10
+for epoch in range(num_epochs):
+    # Training
+    train_loss = 0.0
+    train_acc = 0.0
+    model.train()
+    for batch_idx, (df, target) in enumerate(train_loader):
+        optimizer.zero_grad()
+        outputs = model(df)
+        loss = criterion(outputs, target)
+        loss.backward()
+        optimizer.step()
+        train_loss += loss.item() * df.size(0)
+        _, pred = torch.max(outputs, 1)
+        train_acc += torch.sum(pred == target.data)
+
+    train_loss = train_loss / len(train_loader.dataset)
+    train_acc = train_acc / len(train_loader.dataset)
+
+    # Validation
+    val_loss = 0.0
+    val_acc = 0.0
+    model.eval()
+    with torch.no_grad():
+        for batch_idx, (df, target) in enumerate(val_loader):
+            outputs = model(df)
+            loss = criterion(outputs, target)
+            val_loss += loss.item() * df.size(0)
+            _, pred = torch.max(outputs, 1)
+            val_acc += torch.sum(pred == target.data)
+
+    val_loss = val_loss / len(val_loader.dataset)
+    val_acc = val_acc / len(val_loader.dataset)
+
+    print(
+        f"Epoch {epoch+1}/{num_epochs}, Train Loss: {train_loss:.4f}, Train Acc: {train_acc:.4f}, Val Loss: {val_loss:.4f}, Val Acc: {val_acc:.4f}"
+    )
 
 # %% [markdown]
 # # Graphical Testing
@@ -465,10 +667,10 @@ plt.plot(model.history.history["val_accuracy"])
 # %%
 y_pred = model(x_data)
 
-# %% colab={"base_uri": "https://localhost:8080/"}
+# %%
 print(np.unique(y_pred))
 
-# %% colab={"base_uri": "https://localhost:8080/", "height": 265}
+# %%
 start = 77450
 interval = 1900
 
@@ -479,7 +681,7 @@ plt.plot(y_data[start : start + interval])
 plt.plot(scaled)
 plt.show()
 
-# %% colab={"base_uri": "https://localhost:8080/", "height": 372}
+# %%
 y_smoother = []
 delta = 5
 theshold = 0.75
@@ -514,7 +716,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from mpl_toolkits import mplot3d
 
-# %% colab={"background_save": true, "base_uri": "https://localhost:8080/", "height": 596}
+# %%
 delta_i = 800
 
 n = 1000 - delta_i
@@ -578,16 +780,16 @@ anim = animation.FuncAnimation(
 )
 # plt.savefig('./h=0.02.pdf', format='pdf', dpi=1200,bbox_inches = 'tight')
 
-# %% colab={"background_save": true, "base_uri": "https://localhost:8080/", "height": 822}
+# %%
 anim
 
-# %% colab={"base_uri": "https://localhost:8080/", "height": 283}
+# %%
 plt.plot(freq_list[:], x_test[59])
 
 # %% [markdown]
 # Testing with validation data
 
-# %% colab={"background_save": true}
+# %%
 x_valid_copy = x_valid
 y_valid_copy = y_valid
 
@@ -599,11 +801,11 @@ y_valid = np.array(y_valid)
 x_valid = x_valid.reshape(x_valid.shape[0], x_valid.shape[1], 1)
 y_valid = y_valid.reshape(y_valid.shape[0], 1)
 
-# %% colab={"base_uri": "https://localhost:8080/"}
+# %%
 valid_pred = model(x_valid)
 print(np.unique(valid_pred))
 
-# %% colab={"base_uri": "https://localhost:8080/", "height": 266}
+# %%
 start = 9970
 interval = 1600
 
@@ -629,14 +831,14 @@ y_valid = y_valid.reshape(y_valid.shape[1], 1)
 
 # %% [markdown]
 # Prayer
-
-from sklearn.ensemble import RandomForestClassifier
+#
+# from sklearn.ensemble import RandomForestClassifier
 # %%
 from sklearn.linear_model import LogisticRegression
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
 
-# %% colab={"base_uri": "https://localhost:8080/", "height": 380}
+# %%
 # Option 1: Logistic Regression
 lr = LogisticRegression(max_iter=1000)
 lr.fit(x_train, y_train)
